2068c664b201f9b1276d13d3820ef2849a21d44b
grac2801 <oscar.gracias@icloud.com>
2024-09-13 14:22:07 -0700

    input and data types

---
+++ unit_001/U1_L6_input.py
@@ -0,0 +1,34 @@
+'''
+2024-09-13
+ogracias
+title = input from user
+'''
+# print('********************')
+# print('introduction to input')
+# print('********************')
+# name = input('What is your name? ')
+# print('Good afternoon ' + name + '. Nice to meet you.')
+# print('Good afternoon ', name, '. Nice to meet you.', sep ='')
+
+
+#===============================================================================
+# Activity:
+# Ask for a name, a wild animal, adjective
+# say --> Hello {name}. I went to the zoo to see {wildAnimal}. It looked {adjective}
+#===============================================================================
+# name = input('Enter a name: ')
+# wildAnimal = input('Enter the name of a wild animal: ')
+# adjective = input('Enter an adjective: ')
+# print('Hello ' + name + '. I just saw a(n) ' + wildAnimal + '. It looked ' + adjective)
+# print(f'Hello {name}. I just saw a(n) {wildAnimal}. It looked {adjective}')
+
+
+print('\n\n********************')
+print('variables name rules')
+print('********************')
+#===============================================================================
+# 1- they can use letters and numbers, but not spaces --> firstDay not--> first day
+# 2- they must start with a lowercase letter
+# 3- Use descriptive names. Instead of x, use dayOfTheWeek
+# 4- Use camel case [dayOfTheWeek] or underscores --> days_of_the_week
+#===============================================================================
---
+++ unit_001/U1_L7_dataTypes.py
@@ -0,0 +1,50 @@
+'''
+2024-09-13
+ogracias
+title = data types [int, float, strings]
+'''
+# print('\n\n********************')
+# print('Adding 2 numbers')
+# print('********************')
+# num1 = input('Enter a number: ')
+# num2 = input('Enter another number: ')
+# print(num1 + num2)
+
+
+
+# print('\n\n********************')
+# print('Adding 2 numbers')
+# print('********************')
+# num1 = int(input('Enter a number: '))
+# num2 = int(input('Enter another number: '))
+# print(num1 + num2)
+
+
+
+
+# print('\n\n********************')
+# print('print a number')
+# print('********************')
+# anotherNumber = int(input('Enter a number: '))
+# print('My number is ' + str(anotherNumber))
+# print(f'My number is {anotherNumber}')
+
+
+
+
+#===============================================================================
+# activity:
+# Ask for a color, and integer, and make a statement with them using
+# the special formatting
+#===============================================================================
+
+print('\n\n********************')
+print('floats [decimal numbers]')
+print('********************')
+dec1 = float(input('Enter a decimal value: '))
+dec2 = float(input('Enter another decimal value: '))
+total = dec1 + dec2
+print(f'My total from both decimals is: {total}')
+print(type(total))
+
+
