0efac7f7fd0b85d2d64e2bd88df4a948f4d89745
ogracias <oscar.gracias@icloud.com>
2024-10-21 15:41:52 -0700

    Uncommented U4 lessons 2 and 3

--- unit_004_iteration/U4_L3_waysToEndALoop.py
+++ unit_004_iteration/U4_L3_waysToEndALoop.py
@@ -7,28 +7,28 @@ title = ways to end a while loop
 # imports
 import random
 
-# print('\n\n*******************')
-# print('#1- counter or user input')
-# print('*******************')
-# count = 1
-# while(count <= 10):
-#     print(f'{count:4}: Python')
-#     count += 1
-#
-#
-# print('\n\n*******************')
-# print('#2 - Ask for iteration but check with an if statement')
-# print('*******************')
-# counter = 0
-# totalSum = 0
-#
-# while(counter < 5):
-#     n = int(input('Enter a number: '))
-#     if(n % 3 == 0):
-#         print(str(n) + ' is divisible by 3')
-#         totalSum += n
-#     counter += 1
-# print('the total sum of %3:', totalSum)
+print('\n\n*******************')
+print('#1- counter or user input')
+print('*******************')
+count = 1
+while(count <= 10):
+    print(f'{count:4}: Python')
+    count += 1
+
+
+print('\n\n*******************')
+print('#2 - Ask for iteration but check with an if statement')
+print('*******************')
+counter = 0
+totalSum = 0
+
+while(counter < 5):
+    n = int(input('Enter a number: '))
+    if(n % 3 == 0):
+        print(str(n) + ' is divisible by 3')
+        totalSum += n
+    counter += 1
+print('the total sum of %3:', totalSum)
 
 
 #===================================================================================================
@@ -37,19 +37,19 @@ import random
 # of the numbers input. 
 # When printing, format answer to 2 digits after the decimal point
 #===================================================================================================
-# counter = 0
-# addition = 0
-#
-# n = int(input('Enter an integer. (-1 to stop): '))
-# if n == -1:
-#     print('-1 input. Script will end.')
-# else:
-#     while(n != -1):
-#         counter += 1
-#         addition += n
-#         n = int(input('Enter an integer. (-1 to stop): '))
-#     average = (addition / counter)
-#     print(f'The average is: {average:.2f}')
+counter = 0
+addition = 0
+
+n = int(input('Enter an integer. (-1 to stop): '))
+if n == -1:
+    print('-1 input. Script will end.')
+else:
+    while(n != -1):
+        counter += 1
+        addition += n
+        n = int(input('Enter an integer. (-1 to stop): '))
+    average = (addition / counter)
+    print(f'The average is: {average:.2f}')
     
 print('\n\n*******************')
 print('random iteration')
