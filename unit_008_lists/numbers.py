fa276c69c38aa1a3ee4f78eb86bb415cf324054d
ogracias <oscar.gracias@icloud.com>
2024-12-02 14:08:08 -0800

    File with ord and chr for password assignment

---
+++ unit_008_lists/numbers.py
@@ -0,0 +1,20 @@
+
+
+# imports
+
+
+# global variables | constants
+
+
+# functions
+
+    
+
+
+
+
+if __name__ == '__main__':
+    print('A')
+    print(ord('A'))
+    print(67 + 32)
+    print(chr(67 + 32))
